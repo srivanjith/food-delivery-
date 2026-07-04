ce037099f505d9d96cfa0e38d99244c7aaa27056
SRI VANJITH T <srivanjiththangaraj48@gmail.com>
2026-07-04 10:06:13 +0530

    Create .env.example templates and configure dotenv for backend server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import { getData, saveData } from './db.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
